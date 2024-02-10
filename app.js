a5dcbb82a599060ad3e4cc907755eccc413e3972
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-10 21:32:16 +0400

    Remove files that are now in .gitignore

--- app.js
+++
@@ -1,5 +0,0 @@
-import { generateReportData, storeData } from './src/data.js';
-import log from './src/util/logger.js';
-
-const data = generateReportData(log);
-storeData(data);
--- src/auth.js
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export function generateToken(userEmail, doneFn) {
-  jwt.sign({ email: userEmail }, 'secret123', doneFn);
-}
