7b61b5e79effdd5bd5017110131b53c93ee97063
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-10 21:24:14 +0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+import writeData from './util/io.js';
+
+export function generateReportData(logFn) {
+  const data = 'Some dummy data for this demo app';
+  
+  if (logFn) logFn(data)
+
+  return data;
+}
+
+export async function storeData(data) {
+  if (!data) {
+    throw new Error('No data received!');
+  }
+  await writeData(data, 'data.txt');
+}
