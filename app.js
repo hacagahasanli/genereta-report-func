07587b5b525435c7772ab2ef3719a1c66fd9eab2
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-11 22:25:56 +0400

    creating testing file for io.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import { generateReportData, storeData } from './src/data.js';
 import log from './src/util/logger.js';
 
+import { generateReportData, storeData } from './src/data.js';
+
 const data = generateReportData(log);
+
 storeData(data);
