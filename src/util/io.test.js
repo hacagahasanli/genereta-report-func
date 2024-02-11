d0271a184a2c1a5bea507a83fa7b3e3f739ee55b
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-11 23:15:57 +0400

    using spies (vi.mock) to mock our join method setting our own implmentation as a second argument of vi.mock method

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -5,13 +5,23 @@ import { promises as fs } from 'fs';
 import writeData from './io';
 
 vi.mock('fs');
+// function to mock method, this allows you to set your own implementation , your own replacement
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 it('should execute the writeFile method', () => {
   const testData = 'Test';
   const testFilename = 'test.txt';
 
-  writeData(testData, testFilename)
-  
+  writeData(testData, testFilename);
+
   // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
-  expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 });
